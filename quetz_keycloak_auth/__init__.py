edeef01714bc57605fb5daac336f8891a311db70
Nicolas Misk <nicolas.misk@rayference.eu>
2022-06-16 13:17:41 +0000

    Fix Keycloak authentication flow using openid

--- quetz_keycloak_auth/__init__.py
+++ quetz_keycloak_auth/__init__.py
@@ -1,6 +1,6 @@
 from quetz.authentication.oauth2 import OAuthAuthenticator
 from quetz.config import Config, ConfigEntry, ConfigSection
-import logging
+import json
 
 
 class KeycloakAuthenticator(OAuthAuthenticator):
@@ -32,8 +32,9 @@ class KeycloakAuthenticator(OAuthAuthenticator):
             self.realm_url = f'{config.keycloak_url}/auth/realms/{self.realm}'
             self.access_token_url = f'{self.realm_url}/protocol/openid-connect/token'
             self.authorize_url = f'{self.realm_url}/protocol/openid-connect/auth'
-            self.revoke_url = f'{self.realm_url}/protocol/openid-connect/revoke'
+            self.revoke_url = f'{self.realm_url}/protocol/openid-connect/logout'
             self.userinfo_url = f'{self.realm_url}/protocol/openid-connect/userinfo'
+            self.validate_token_url = f'{self.realm_url}/protocol/openid-connect/auth'
             self.client_id = config.keycloak_client_id
             self.client_secret = config.keycloak_client_secret
             self.is_enabled = True
@@ -52,7 +53,7 @@ class KeycloakAuthenticator(OAuthAuthenticator):
         resp = await self.client.get(self.userinfo_url, token=token)
         profile = resp.json()
 
-        # TODO: better avatar implementation
+        # TODO: Avatar implementation
         profile = {
             "id": profile["sub"],
             "name": profile["name"],
@@ -63,4 +64,17 @@ class KeycloakAuthenticator(OAuthAuthenticator):
 
         return profile
 
+    async def authenticate(self, request, data=None, dao=None, config=None):
+        token = await self.client.authorize_access_token(request)
+        profile = await self.userinfo(request, token)
+
+        username = profile["login"]
+
+        # Filtering the refresh token so we can save the token in a cookie
+        # session
+        token.pop('refresh_token', None)
+
+        auth_state = {"token": json.dumps(token), "provider": self.provider}
+
+        return {"username": username, "profile": profile, "auth_state": auth_state}
 
